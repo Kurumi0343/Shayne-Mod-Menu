e33d92233fb965278f3a1206d037522866779cbc
Rachel <rachelgardner0343@gmail.com>
2022-06-19 01:39:26 +0800

    test

--- app/src/main/jni/Menu/Setup.h
+++ app/src/main/jni/Menu/Setup.h
@@ -113,7 +113,7 @@ void Init(JNIEnv *env, jobject thiz, jobject ctx, jobject title, jobject subtitl
 
     //Set sub title
     setText(env, subtitle, OBFUSCATE("<b><marquee><p style=\"font-size:30\">"
-                                     "<p style=\"color:green;\">Modded by LGL</p> | "
+                                     "<p style=\"color:green;\">Modded by Shayne</p> | "
                                      "</marquee></b>"));
 
     //Dialog Example
